7b8681d16c661d7ea7b185158bf0b01563812b64
kazuki827 <xm.kazuki827@gmail.com>
2022-05-09 21:59:29 +0900

    project setup

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Home from '../pages'
+
+it('Should render hello text', () => {
+  render(<Home />)
+  expect(screen.getByText('Hello Next.js')).toBeInTheDocument()
+})
