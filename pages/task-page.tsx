20ef71b4eb578815bd851dc96f7c771c10bdab82
kazuki827 <xm.kazuki827@gmail.com>
2022-05-11 11:05:07 +0900

    create task page

--- pages/task-page.tsx
+++ pages/task-page.tsx
@@ -1,11 +1,50 @@
+import { GetStaticProps } from 'next'
+import axios from 'axios'
 import Layout from '../components/Layout'
+import { TASK } from '../types/types'
+import { getAllTasksData } from '../lib/fetch'
+import useSWR from 'swr'
 
-const TaskPage: React.FC = () => {
+interface STATICPROPS {
+  staticTasks: TASK[]
+}
+
+const axiosFetcher = async () => {
+  const result = await axios.get<TASK[]>(
+    'https://jsonplaceholder.typicode.com/todos/?_limit=10'
+  )
+  return result.data
+}
+
+const TaskPage: React.FC<STATICPROPS> = ({ staticTasks }) => {
+  const { data: tasks, error } = useSWR('todoFetch', axiosFetcher, {
+    fallbackData: staticTasks,
+    revalidateOnMount: true,
+  })
+
+  if (error) return <span>Error!</span>
+
   return (
     <Layout title="Task">
-      <p className="text-4xl">task page</p>
+      <p className="text-4xl mb-10">task page</p>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.id}
+            {': '}
+            <span>{task.title}</span>
+          </li>
+        ))}
+      </ul>
     </Layout>
   )
 }
 
 export default TaskPage
+
+export const getStaticProps: GetStaticProps = async () => {
+  const staticTasks = await getAllTasksData()
+  return {
+    props: { staticTasks },
+  }
+}
